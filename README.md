57361e501f5e53204a2968b8a38d3ade962e7e52
John Blischak <jdblischak@gmail.com>
2026-05-15 10:22:47 -0400

    Simplify download of mingw64_ucrt64 release binary (#8)

--- README.md
+++ README.md
@@ -4,40 +4,22 @@
 This repository is a fork of the one by Jeroen Ooms allowing us to proceed
 without relying on him.  This repository serves two purposes
 
-- gather the artifacts from the GitHub Actions 'rtools4' build step of TileDB Embedded
+- download the release artifact from the GitHub Release page
 - re-arrange the files such that the build step of the TileDB-R package can use them
-  
+
 ### Details
 
-The following is from a local script and reflects the Unix desktop use of `~/Downloads`. Adjust
-accordingly -- "eventually" we may push the artifacts somewhere where a fixed URL will allow a 
-download via `curl` or `wget`.
+As of release [2.28.0-rc0][] on 2025-05-06, the mingw64_ucrt64 release binary
+have been included as GitHub Release artifact. This greater simplifies the
+download process since it is now a stable URL.
+
+[2.28.0-rc0]: https://github.com/TileDB-Inc/TileDB/releases/tag/2.28.0-rc0
+
+To update the files in this repository for a new release of TileDB, run:
 
 ```sh
-#!/bin/bash
+bash update_ucrt_only.sh TAG SHA
 
-## From the rtools4 (and *not* msys2 which is the next compiler set,
-## aka what may be used for 'r-devel') build: download the two artifacts
-## to the ~/Downloads/ directory (i.e. the default location on Ubuntu)
-## eg for 2.2.2 the URL is https://github.com/TileDB-Inc/TileDB/runs/1766128541
-for f in mingw-w64-i686-tiledb.zip mingw-w64-x86_64-tiledb.zip; do
-    mv -v ~/Downloads/${f} .
-    unzip ${f}
-    rm ${f}
-done
-
-tar xaf mingw-w64-i686-tiledb-2.9000-1-any.pkg.tar.xz
-rsync -cav mingw32/include/tiledb/ include/tiledb/
-rsync -cav mingw32/lib/libtiledbstatic.a lib/i386/libtiledbstatic.a
-rm -rf mingw32
-rm -v mingw-w64-i686-tiledb-2.9000-1-any.pkg.tar.xz
-
-tar xaf mingw-w64-x86_64-tiledb-2.9000-1-any.pkg.tar.xz
-rsync -cav mingw64/include/tiledb/ include/tiledb/
-rsync -cav mingw64/lib/libtiledbstatic.a lib/x64/libtiledbstatic.a
-rm -rf mingw64
-rm -v mingw-w64-x86_64-tiledb-2.9000-1-any.pkg.tar.xz
-
-rm .BUILDINFO .MTREE .PKGINFO
+# For example
+bash update_ucrt_only.sh 2.30.1 6ea48ca
 ```
-  
--- update_ucrt_only.sh
+++ update_ucrt_only.sh
@@ -1,46 +1,24 @@
 #!/bin/bash
+set -eux
 
-set -x
+# Usage: bash update_ucrt_only.sh TAG SHA
+#
+# where
+# tag=X.X.X
+# SHA=xxxxxxx
 
-## From the rtools4 (and *not* msys2 which is the next compiler set,
-## aka what may be used for 'r-devel') build: download the two artifacts
-## to the ~/Downloads/ directory (i.e. the default location on Ubuntu)
-## eg for 2.2.2 the URL is https://github.com/TileDB-Inc/TileDB/runs/1766128541
-##
-## And now also updated for 'ucrt' builds
-##
-for f in release-windows-x86_64-mingw64_ucrt64-2.30.0-9b5305d.zip; do
-#for f in mingw-w64-MINGW32-tiledb.zip mingw-w64-MINGW64-tiledb.zip mingw-w64-ucrt64-tiledb.zip; do
-    cp ~/Downloads/${f} .
-    unzip ${f}
-    rm ${f}
-done
+# Setup
+TAG="$1"
+SHA="$2"
+tarball="tiledb-windows-x86_64-mingw64_ucrt64-${TAG}-${SHA}.tar.xz"
+url="https://github.com/TileDB-Inc/TileDB/releases/download/${TAG}/"
 
-# tar xaf mingw-w64-i686-tiledb-2.9000-1-any.pkg.tar.xz
-# ver=$(strings lib/x64/libtiledbstatic.a | awk '/^GCC: \(Built by Jeroen/ {print $8; exit}')
-# if [ ${ver} != "8.3.0" ]; then
-#     echo "Wrong format. Exiting."
-#     exit -1
-# fi
-# rsync -cav mingw32/include/tiledb/ include/tiledb/
-# #rsync -cav mingw32/lib/libtiledbstatic.a lib/i386/libtiledbstatic.a
-# rsync -cav mingw32/lib/lib*.a lib/i386/
-# rm -rf mingw32
-# rm -v mingw-w64-i686-tiledb-2.9000-1-any.pkg.tar.xz
-
-
-# tar xaf mingw-w64-x86_64-tiledb-2.9000-1-any.pkg.tar.xz
-# rsync -cav mingw64/include/tiledb/ include/tiledb/
-# #rsync -cav mingw64/lib/libtiledbstatic.a lib/x64/libtiledbstatic.a
-# rsync -cav mingw64/lib/lib*.a lib/x64/
-# rm -rf mingw64
-# rm -v mingw-w64-x86_64-tiledb-2.9000-1-any.pkg.tar.xz
-
-tar xaf tiledb-windows-x86_64-mingw64_ucrt64-2.30.0-9b5305d.tar.xz
+# Download, extract, and copy
+wget "$url/$tarball"
+tar xaf "$tarball"
 rsync -cav ucrt64/include/tiledb/ include/tiledb/
 #rsync -cav ucrt64/lib/libtiledbstatic.a lib/x64-ucrt/libtiledbstatic.a
 rsync -cav ucrt64/lib/lib*.a lib/x64-ucrt/
-rm -rf ucrt64
-rm -v tiledb-windows-x86_64-mingw64_ucrt64-2.30.0-9b5305d.tar.xz
 
-rm .BUILDINFO .MTREE .PKGINFO
+# Cleanup
+rm -r "$tarball" ucrt64/ .BUILDINFO .MTREE .PKGINFO
